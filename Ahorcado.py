ec18aa7001260dedd086d5de4427dceba2970ca3
daagonzalezgu <daagonzalezgu@unal.edu.co>
2021-01-26 13:00:46 -0500

    Prueba de la funcion display para imprimir las letras incorrestas

--- Ahorcado.py
+++ Ahorcado.py
@@ -62,35 +62,17 @@ def seleccionar_palabra(palabras):
 
 #Dibujar tablero
 def display(tablero,palabra_secreta, letra_correcta, letra_incorrecta):
-     print(tablero[len(palabra_incorrecta)])
+     #Muestra el tablero con el ahorcado
+     print(tablero[len(letra_incorrecta)])
+     #Muestra las letras incorrectas hasta el momento
+     print('Letras incorrectas: ', ' ')
+     for letra in letra_incorrecta:
+          print(letra,' ')
      
 
 
-#pedir una letra 
-def dar_letra():
-     letra = str(input())
-#buscar la letra
-#reemplazar en el tablero 
-print(seleccionar_palabra(palabras))
-#print(tablero[3])
-
-#Bienvenida
-     intro = """
-     Ahorcado 1.0
-     Bienvenid@ al menu principal
-     ¿Estás list@ para empezar?
-     """
-#Seleccionar palabra
 palabra_secreta = seleccionar_palabra(palabras)
-letras_correctas = []
-letras_incorrectas = []
-while True:
-     #dibujar tablero
-     display(tablero, palabra_secreta, letra_correcta, letra_incorrecta)
-     
-     #Pedir letra
-     #Buscar letra
-          
-     #Fin del juego ?
-     #Preguntar si jugar de nuevo
+letra_correcta = []
+letra_incorrecta = ['a','b','c','d']
+display(tablero,palabra_secreta,letra_correcta,letra_incorrecta)
 
